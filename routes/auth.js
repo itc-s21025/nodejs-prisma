cab5d64a1c057dbd7ded66a6e3d8bb05f5b77a59
itc-s21025 <s21025@std.it-college.ac.jp>
2023-02-22 17:22:46 +0900

    sessionで躓いています

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const {PrismaClient} = require('@prisma/client');
-const prisma = new PrismaClient();
 
+const prisma = new PrismaClient();
 
 router.post("/register",async (req, res) => {
     const data = {data: req.body};
@@ -15,6 +15,7 @@ router.post("/register",async (req, res) => {
 
 // ログイン
 router.post('/login', async (req, res) => {
+
     const {code} = req.body
     const studentid = await prisma.student.findFirst({
         where: {
@@ -25,9 +26,13 @@ router.post('/login', async (req, res) => {
         res.json({status: 0})
     }else {
         res.json({status: 1})
+        res.session.status
     }
 });
 
-
+router.post('/logout', async  (req, res) => {
+    res.session.studentId
+    res.json({status: 0})
+});
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const auth = require("./routes/auth");
-
-
-
-
+const session = require('express-session')
+app.set('trust proxy', 1)
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
 app.use(express.json());
 app.use("/", auth);
 app.listen(PORT, () => {
