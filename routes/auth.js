07a23d2633a744c323a45052406c5c2795f7b2a5
itc-s21025 <s21025@std.it-college.ac.jp>
2023-02-23 15:07:46 +0900

    sessionで躓いています2

--- routes/auth.js
+++ routes/auth.js
@@ -1,38 +1,62 @@
 const router = require("express").Router();
 const {PrismaClient} = require('@prisma/client');
-
 const prisma = new PrismaClient();
 
-router.post("/register",async (req, res) => {
+
+router.post("/register", async (req, res) => {
     const data = {data: req.body};
     await prisma.student.create(data)
-        .then(() => {
+    .then(() => {
         res.json({status: 0})
     }).catch(() => {
         res.json({status: 1})
     })
 })
 
+
 // ログイン
 router.post('/login', async (req, res) => {
-
-    const {code} = req.body
-    const studentid = await prisma.student.findFirst({
+    const student_code = {data: req.body};
+    const student = await prisma.student.findFirst({
         where: {
-            code: code
+            code: student_code
         }
     })
-    if (studentid == null) {
+    if (student == null) {
         res.json({status: 0})
     }else {
+        req.session.student = student
         res.json({status: 1})
-        res.session.status
     }
-});
+})
+/**const data = {data: req.body};
+ const studentid = await prisma.student.findFirst({
+        where: {
+            code: data
+        }
+    })
+ if (studentid == null) {
+        res.json({status: 0})
+    }else {
+        req.session.studentid = studentid
+        res.json({status: 1})
+    }
+ });
+ **/
 
-router.post('/logout', async  (req, res) => {
+router.post('/logout', async (req, res) => {
     res.session.studentId
     res.json({status: 0})
 });
+
+router.get('/test', async (req, res) => {
+    const data = "s21022"
+    const student = await prisma.student.findFirst({
+        where: {
+            code: data
+        }
+    })
+    console.log(student)
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const bodyParser = require('body-parser')
 const auth = require("./routes/auth");
 const session = require('express-session')
-app.set('trust proxy', 1)
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true }
-}))
+const pgSession = require('connect-pg-simple')(session);
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.json());
 app.use("/", auth);
+
+app.set('trust proxy', 1)
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "qawsedrf",
+    store: new (require('connect-pg-simple')(session))({
+        conString: process.env.DATABASE_URL,
+        createTableIfMissing: true
+    })
+
+}));
+
 app.listen(PORT, () => {
     console.log("サーバーを起動中");
 
